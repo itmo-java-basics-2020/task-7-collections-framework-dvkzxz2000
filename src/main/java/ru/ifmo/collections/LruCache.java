ffc8ce69f46a1e722b431fa5536628a78e6de8f3
dvkzxz2000 <dvk.zxz@gmail.com>
2020-06-14 19:21:58 +0300

    last

--- src/main/java/ru/ifmo/collections/LruCache.java
+++ src/main/java/ru/ifmo/collections/LruCache.java
@@ -1,5 +1,8 @@
 package ru.ifmo.collections;
 
+import java.util.Map;
+import java.util.LinkedHashMap;
+
 /**
  * Represents LRU cache with fixed maximum capacity.
  *
@@ -11,19 +14,27 @@ package ru.ifmo.collections;
  * Implementing this cache in (almost) the same manner as it was implemented during the lecture will result in extra points.
  */
 public class LruCache<K, V> {
-    public LruCache(int capacity) {
-        // TODO implement
+
+    private LinkedHashMap<K, V> kvMap;
+
+    public LruCache(int x) {
+        kvMap = new LinkedHashMap<>(x, 0.75f, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<K, V> y) {
+                return size() > x;
+            }
+        };
     }
 
     public V get(K key) {
-        throw new UnsupportedOperationException(); // TODO implement
+        return kvMap.get(key);
     }
 
     public void put(K key, V value) {
-        // TODO implement
+        kvMap.put(key, value);
     }
 
     public int elements() {
-        throw new UnsupportedOperationException(); // TODO implement
+        return kvMap.size();
     }
 }
